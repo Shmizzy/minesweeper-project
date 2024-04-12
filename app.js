ec24fb57b6fb44bd77164db78e56eddf300ece60
izzylppz <https://github.com/Shmizzy>
2024-04-11 23:08:18 -0500

    added grid

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 
+
 let gridArray = [];
+const adjLocations = [
+    {adjX: -1, adjY: -1}, {adjX: -1, adjY: 0}, {adjX: -1, adjY: 1},
+    {adjX: 0, adjY: -1},{adjX: 0, adjY: 1},
+    {adjX: 1, adjY: -1}, {adjX: 1, adjY: 0}, {adjX: 1, adjY: 1},
+]
+
 const button = document.querySelector('#start-button');
-const gameGrid = document.querySelector('#game-grid');
+const gameGrid = document.getElementById('game-grid');
 
 button.addEventListener('click', ()=> startGame());
 
 const generateGrid = () => {
   
-    
+    document.getElementById('game-screen').style.display = 'grid'
     for(let i = 0; i < 10; i++){
         gridArray[i] = []
         for(let j = 0; j < 10; j++){
             const tile = document.createElement('div');
             tile.classList.add(`unrevealed`);
             tile.dataset.xy = `${i},${j}`;
-            tile.innerHTML = `${i},${j}`;
+            
             gridArray[i][j] = {
                 adjacentBombs: 0,
                 isRevealed: false,
@@ -22,12 +29,7 @@ const generateGrid = () => {
             };
 
             gameGrid.appendChild(tile);
-
             tile.addEventListener('click', () => reveal(i, j));
-
-
-
-
         }
     }
 
@@ -51,11 +53,24 @@ const reveal = (x,y) => {
         if(gridArray[x][y].isBomb === false){
             grabTile.classList.replace('unrevealed','revealed');
             gridArray[x][y].isRevealed = true;
-            checkAdjacentTiles(x,y);
+            const bombCount = checkAdjacentTiles(x,y);
+            if(bombCount !== 0){
+                grabTile.innerHTML = bombCount;
+            }else {
+                for(let adj of adjLocations){
+                    const newX = x + adj.adjX;
+                    const newY = y + adj.adjY;
+            
+                    if(newX >= 0 && newX < 10 && newY >= 0 && newY < 10){
+                        reveal(newX,newY);
+                    }
+                }
+            }
         }else{
             grabTile.classList.replace('unrevealed','bomb');
             gridArray[x][y].isRevealed = true;
             revealAllBombs();
+           
         }
     }
     
@@ -64,6 +79,19 @@ const reveal = (x,y) => {
 const checkAdjacentTiles = (x,y) => {
 
 
+    adjBombCount = 0;
+    
+
+    for(let adj of adjLocations){
+        const newX = x + adj.adjX;
+        const newY = y + adj.adjY;
+
+        if(newX >= 0 && newX < 10 && newY >= 0 && newY < 10){
+            if(gridArray[newX][newY].isBomb)adjBombCount++;
+        }
+    }
+
+    return adjBombCount;
 
 }
 
@@ -87,7 +115,31 @@ const revealAllBombs = () => {
 const startGame = () => {
 
     document.getElementById('dialog').style.display = 'none';
-    document.getElementById('game-screen').style.display = 'grid'
     
     generateGrid();
+}
+
+const endGame = () =>{
+    document.getElementById('game-screen').style.display = 'none'
+    const body = document.querySelector('body');
+    const endDiv = document.createElement('div');
+    const gameOver = document.createElement('h1');
+    const playAgain = document.createElement('button');
+    endDiv.classList.add('end-dialog');
+    playAgain.classList.add('end-button');
+
+    gameOver.textContent = "Game Over!!!"
+    playAgain.textContent = "Play Again???"
+
+    playAgain.addEventListener('click', ()=>{
+        endDiv.style.display = 'none';
+        gameGrid.innerHTML = '';
+        gridArray = [];
+        startGame();
+    });
+
+    
+    endDiv.appendChild(gameOver);
+    endDiv.appendChild(playAgain);
+    body.appendChild(endDiv);
 }
