c2cb905a1cfb206f124bcee829f54c82f665328c
izzylppz <https://github.com/Shmizzy>
2024-04-16 11:50:17 -0500

    reveal all

--- app.js
+++ app.js
@@ -1,58 +1,74 @@
+const startButton = document.querySelector('#start-button');
+const dialogElement = document.getElementById('dialog');
+const gameElement = document.getElementById('game-screen');
+const gridElement = document.getElementById('game-grid');
 
-
-let gridArray = [];
+let scoresList = [];
+let totalGamesStarted = [];
 const adjLocations = [
     {adjX: -1, adjY: -1}, {adjX: -1, adjY: 0}, {adjX: -1, adjY: 1},
     {adjX: 0, adjY: -1},{adjX: 0, adjY: 1},
     {adjX: 1, adjY: -1}, {adjX: 1, adjY: 0}, {adjX: 1, adjY: 1},
 ]
 
-const button = document.querySelector('#start-button');
-const gameGrid = document.getElementById('game-grid');
+startButton.addEventListener('click', () => startGame());
 
-button.addEventListener('click', ()=> startGame());
+
+
+const startGame = () => {
+    
+    dialogElement.style.display = 'none';
+    gameElement.style.display = 'grid';
+    generateGrid();
+
+    
+
+}
+
+
+
 
 const generateGrid = () => {
-  
-    document.getElementById('game-screen').style.display = 'grid'
+    let grid = [];
     for(let i = 0; i < 10; i++){
-        gridArray[i] = []
+        grid.push([]);
         for(let j = 0; j < 10; j++){
+            grid[i][j] = `${i},${j}`;
             const tile = document.createElement('div');
             tile.classList.add(`unrevealed`);
             tile.dataset.xy = `${i},${j}`;
-            
-            gridArray[i][j] = {
+            grid[i][j] = {
                 adjacentBombs: 0,
                 isRevealed: false,
                 isBomb: false
             };
-
-            gameGrid.appendChild(tile);
-            tile.addEventListener('click', () => reveal(i, j));
+            gridElement.appendChild(tile);
+            tile.addEventListener('click', () => reveal(i,j));
         }
     }
-
+    
     for(let bomb = 0; bomb <= 20; bomb++){
         let randomRow = Math.floor(Math.random() * 10);
         let randomColumn = Math.floor(Math.random() * 10);
-        gridArray[randomRow][randomColumn].isBomb = true;
+        grid[randomRow][randomColumn].isBomb = true;
     }
-   
-
-    console.log(gridArray)
-
+    totalGamesStarted.push(grid);
 }
+console.log(totalGamesStarted);
+
+
+
 
 
 const reveal = (x,y) => {
     
     const grabTile = document.querySelector(`.unrevealed[data-xy="${x},${y}"]`);
+    const grabMainArray = totalGamesStarted[0];
     
-    if(!gridArray[x][y].isRevealed){
-        if(gridArray[x][y].isBomb === false){
+    if(!grabMainArray[x][y].isRevealed){
+        if(grabMainArray[x][y].isBomb === false){
             grabTile.classList.replace('unrevealed','revealed');
-            gridArray[x][y].isRevealed = true;
+            grabMainArray[x][y].isRevealed = true;
             const bombCount = checkAdjacentTiles(x,y);
             if(bombCount !== 0){
                 grabTile.innerHTML = bombCount;
@@ -66,19 +82,18 @@ const reveal = (x,y) => {
                     }
                 }
             }
-        }else{
-            grabTile.classList.replace('unrevealed','bomb');
-            gridArray[x][y].isRevealed = true;
-            revealAllBombs();
+        }else {
+            revealAll();
            
         }
     }
     
 }
 
+
 const checkAdjacentTiles = (x,y) => {
 
-
+    const grabMainArray = totalGamesStarted[0];
     adjBombCount = 0;
     
 
@@ -87,7 +102,7 @@ const checkAdjacentTiles = (x,y) => {
         const newY = y + adj.adjY;
 
         if(newX >= 0 && newX < 10 && newY >= 0 && newY < 10){
-            if(gridArray[newX][newY].isBomb)adjBombCount++;
+            if(grabMainArray[newX][newY].isBomb)adjBombCount++;
         }
     }
 
@@ -95,51 +110,20 @@ const checkAdjacentTiles = (x,y) => {
 
 }
 
-const revealAllBombs = () => {
+const revealAll = () => { 
 
-
+    const grabMainArray = totalGamesStarted[0];
+    
     for(let i = 0; i < 10; i++){
         for(let j = 0; j < 10; j++){
-            const btile = gridArray[i][j]
-            if(btile.isBomb && !btile.isRevealed){
-                const grabBomb = document.querySelector(`.unrevealed[data-xy="${i},${j}"]`);
-                grabBomb.classList.replace('unrevealed','bomb');
-                btile.isRevealed = true;
+            grabMainArray[i][j].isRevealed = true;
+            grabTile = document.querySelector(`.unrevealed[data-xy="${i},${j}"]`)
+            if(grabTile){
+                if(grabMainArray[i][j].isBomb){
+                    grabTile.classList.replace('unrevealed','bomb')
+                }
+                grabTile.classList.replace('unrevealed','revealed')
             }
-            
         }
     }
-}
-
-
-const startGame = () => {
-
-    document.getElementById('dialog').style.display = 'none';
-    
-    generateGrid();
-}
-
-const endGame = () =>{
-    document.getElementById('game-screen').style.display = 'none'
-    const body = document.querySelector('body');
-    const endDiv = document.createElement('div');
-    const gameOver = document.createElement('h1');
-    const playAgain = document.createElement('button');
-    endDiv.classList.add('end-dialog');
-    playAgain.classList.add('end-button');
-
-    gameOver.textContent = "Game Over!!!"
-    playAgain.textContent = "Play Again???"
-
-    playAgain.addEventListener('click', ()=>{
-        endDiv.style.display = 'none';
-        gameGrid.innerHTML = '';
-        gridArray = [];
-        startGame();
-    });
-
-    
-    endDiv.appendChild(gameOver);
-    endDiv.appendChild(playAgain);
-    body.appendChild(endDiv);
 }
